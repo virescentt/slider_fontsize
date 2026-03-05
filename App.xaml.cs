dd4215a871a451c60e201b409b27feea8b0d6ff6
Virescent <163107908+virescentt@users.noreply.github.com>
2026-03-05 23:22:54 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace slider_fontsize
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace slider_fontsize
+{
+    public partial class MainWindow : Window
+    {
+
+        string[] textsList = ["Dzień dobry", "Good morning", "Buenos dias"];
+        int newIndex = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+            MySlider.ValueChanged += MySlider_ValueChanged;
+            OutputText.Content = textsList[newIndex];
+            
+        }
+
+        private void MySlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            OutputText.FontSize = MySlider.Value;
+            SizeText.Content = "Rozmiar: " + (int)MySlider.Value;
+        }
+
+        private void NextText_Click(object sender, RoutedEventArgs e)
+        {
+            newIndex++;
+            OutputText.Content = textsList[newIndex % textsList.Length];
+        }
+    }
+}
